001d81e31a7b0f7b88710e4ab01bdaea880dcd37
Kyungyoon Ha <gkb10a@naver.com>
2020-09-06 14:51:48 +0900

    PG 연동

--- server/controllers/checkout/checkout.ctrl.js
+++ server/controllers/checkout/checkout.ctrl.js
@@ -1,3 +1,5 @@
+const models = require("../../models");
+
 exports.index = (req, res) => {
     let totalAmount = 0; //총결제금액
     let cartList = {}; //장바구니 리스트
@@ -12,4 +14,13 @@ exports.index = (req, res) => {
         }
     }
     res.render("checkout/index.html", { cartList, totalAmount });
+};
+
+exports.post_complete = async (req, res) => {
+    await models.Checkout.create(req.body);
+    res.json({ message: "success" });
+};
+
+exports.get_success = (req, res) => {
+    res.render("checkout/success.html");
 };
